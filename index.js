e0ce125377cea504265ab46fe5618b1dd3ed0904
Andrea Siede <andrea.siede@gmail.com>
2018-08-15 14:22:10 -0700

    final styles

--- index.js
+++ index.js
@@ -6,73 +6,10 @@ const FOURSQUARE_VENUE_SEARCH_URL = 'https://api.foursquare.com/v2/venues/'
 const GRACENOTE_SEARCH_URL = 'https://data.tmsapi.com/v1.1/movies/showings';
 const TMDB_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
 
-
-//
 let restaurantData = [];
+let movieData = []
 
-function handlesRestaurantData(data){
-  const groupsObj = data.response.groups[0];
-  restaurantData = groupsObj.items;
-  getRestaurantPhotos();
-}
-
-
-
-function handlesVenueData(data, index) {
-  console.log(data);
-  restaurantData[index].img = `${data.response.venue.bestPhoto.prefix}100x100${data.response.venue.bestPhoto.suffix}`;
-  
-  function renderHours(hourInfo) {
-    return `<li>${hourInfo.days}: ${hourInfo.open[0].renderedTime}</li>`
-  }
-
-  restaurantData[index].hours = `${(data.response.venue.hours.timeframes.map((hourInfo) => renderHours(hourInfo)))}`;
-}
-
-
-function handlesVenuePlaceholderData() {
-  restaurantData.map(function(restaurant, index) {
-    restaurantData[index].img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Sinnbild_Autobahngasthaus.svg/100px-Sinnbild_Autobahngasthaus.svg.png';
-    restaurantData[index].hours = `check back later for hours`;
-  });
-  displayFOURSQUARESearchData();
-}
-
-
-
-function getRestaurantPhotos() {
-  const requests = restaurantData.map(function(restaurant, index){
-    const restaurantID = restaurantData[index].venue.id;
-    
-
-    return getDataFromFOURSQUAREVENUEApi(restaurantID, index, handlesVenueData);
-  })
-    $.when(...requests).done(() => {
-    displayFOURSQUARESearchData();
-  }) 
-}
-
-// Calculating dates for the Movie API call
-const today = new Date();
-const fullISODate = today.toISOString();
-const ISODate = fullISODate.substr(0,10);
-function createDateMonthAgo() {
-  const date = new Date(); 
-  const todayAMonthAgo = date.setMonth(date.getMonth() -1);
-  return date;    
-  } 
-const fullISODateAMonthAgo = createDateMonthAgo().toISOString();
-const ISODateAMonthAgo = fullISODateAMonthAgo.substr(0,10);
-
-//TMDB's genre IDs
-// const idToGenre = {
-//   27: 'Horror',
-//   18: 'Drama',
-//   10749: 'Fantasy',
-//   35: 'Comedy',
-//   10751: 'Children',
-//   28: 'Action'
-// }
+let theatres = [];
 
 // Apps key of how to match movie genre to food catagory
 const genreToFoodCatagory = {
@@ -84,46 +21,66 @@ const genreToFoodCatagory = {
   "Action": 'diner'
 };
 
+// Calculating dates for the Movie API call
+const today = new Date();
+const fullISODate = today.toISOString();
+const ISODate = fullISODate.substr(0,10);
+const fullISODateAMonthAgo = createDateMonthAgo().toISOString();
+const ISODateAMonthAgo = fullISODateAMonthAgo.substr(0,10);
+function createDateMonthAgo() {
+  const date = new Date(); 
+  const todayAMonthAgo = date.setMonth(date.getMonth() -1);
+  return date;    
+} 
 
 // Beginning the app
 function renderBeginResults() {
   $('.js-search-form').prop('hidden', false);
   $('header').html("<h1>Dinner and a Movie Chooser</h1>");
-  $('.initial-page').prop('hidden', true);
+  $('main').prop('hidden', true);
   $('.edit').hide();
 }
+
 function handleBeginButton() {
   $('.js-begin').on('click', event=>{
     renderBeginResults();
     });
 }
 
-// Handling Movie API
-let movieData = []
+// Handling Movie Results
 
+  //API request for movie posters
+function getDataFromTMDBApi(movieTitle, index, callback) {
+  const query = {
+    query: `${movieTitle}`,
+    api_key: '0d004f603b5e46bc91ac76e45f1a3078',
+  }
+  return $.getJSON(TMDB_SEARCH_URL,  query, (data) => callback(data, index));
+}
+
+  //API request for movies in theaters, titles, descriptions, genres, showtimes
 function getDataFromGRACENOTEApi(zipCode, callback) {
   const query = {
     startDate: `${ISODate}`,
-    api_key: 'wvgp8npjcpddxq2daqde46z3',
+    api_key: 'nvvvccjun8w86gcpzw7apfhk',
     zip: `${zipCode}`
   }
-  $.getJSON(GRACENOTE_SEARCH_URL, query, callback);
+  $.getJSON(GRACENOTE_SEARCH_URL, query, callback)
+  .fail(function(jqXHR, textStatus, errorThrown){
+    //error handling if zip code is not valid zip code
+    if (textStatus == 'parsererror'){
+      $('.error-section').html("<p class='select-error'>The zipcode you have selected is not returning any results. It may be an invalid zipcode.Please try again</p>");
+    } else if (textStatus== 'error'){
+      //error handling if api quota limit has been reached
+      $('.error-section').html("<p class='select-error'>Limit has been reached. Try again later for movie results</p>");
+    }
+  });
 }
 
 function handleMovieData(data){
-  let genreQueryTarget = $('form').find("input[type='radio']:checked");
-  const genre = genreQueryTarget.val();
   const allData = data;
-    function filtersGenre(movie) {
-      return (movie.genres != undefined && movie.genres.includes(genre));
-    }
-
   movieData = allData.filter(filtersGenre).slice(0,3);
   getMoviePhotos();
-}
-
-function handlesMoviePhotoData(data, index) {
-  movieData[index].img = `http://image.tmdb.org/t/p/w185/${data.results[0].poster_path}`;
 }
 
 function getMoviePhotos() {
@@ -135,58 +92,45 @@ function getMoviePhotos() {
   }) 
 }
 
-function renderMovieData(movieObj){
-  const showtimeObj = JSON.stringify(movieObj.showtimes);
-  return  `
-  <div role='button' class='movie-results' data-summary='${movieObj.shortDescription}' data-showtimes='${showtimeObj}'>
-    <h4>${movieObj.title}</h4>
-    <img src='${movieObj.img}' alt=''>
-  </div>
- `; 
-
- // <p>${movieObj.shortDescription}</p>
-
+function handlesMoviePhotoData(data, index) {
+  movieData[index].img = `http://image.tmdb.org/t/p/w185/${data.results[0].poster_path}`;
 }
+
+function filtersGenre(movie) {
+  let genreQueryTarget = $('form').find("input[type='radio']:checked");
+  const genre = genreQueryTarget.val();
+    return (movie.genres != undefined && movie.genres.includes(genre));
+  }
 
 function displayMovieData(data){
   const results = movieData.map((item, index)=>renderMovieData(item));
   $('.js-movie-result h4').html(results);
 }
 
-// TMDB Pic
-
-function getDataFromTMDBApi(movieTitle, index, callback) {
-  const query = {
-    query: `${movieTitle}`,
-    api_key: '0d004f603b5e46bc91ac76e45f1a3078',
-    // 'primary_release_date.gte':`${ISODateAMonthAgo}`,
-    // 'primary_release_date.lte':`${ISODate}`,
-    // sort_by: 'popularity.desc',
-  }
-  return $.getJSON(TMDB_SEARCH_URL,  query, (data) => callback(data, index));
+function renderMovieData(movieObj){
+  const showtimeObj = JSON.stringify(movieObj.showtimes);
+  return  `
+  <div role='button' class='movie-results' data-summary='${movieObj.shortDescription}' data-showtimes='${showtimeObj}'>
+    <h4>${movieObj.title}</h4>
+    <img src='${movieObj.img}' alt='movie poster'>
+  </div>
+ `;
 }
 
-function displayTMDBSearchData(data) {
-  const topThree = data.results.slice(0,3);
-  const results = topThree.map((item, index)=>renderMovieResult(item));
-  $('.js-movie-result h3').append(results);
-}
-
-// Handling FOURSQUARE API---Photo
+//Handles Restaurant Results
+  // Handling FOURSQUARE API---Photo
 function getDataFromFOURSQUAREVENUEApi(restaurantID, index, callback){
   const query = {
-    client_id: 'CQW1ZTGV0JMZAOZKLJZ5SKDWHR54ZQAQP3ERGAASEVGBIJ0Z',
-    client_secret: '2GXXJC0MIY4VBFTIHABBWKNX4XPDKJVKQZAEJFIW5WBYGIGI',
+    client_id: 'JOATRFAR3HQYB2TSPI3MY03ZCIJI0QW4GMPXVCYVPMVJ40WR',
+    client_secret: 'Z0RXLVYULUQVJ0FPYT10NJNFAHZ0NUAUOLWZJPEVJNVJZBQC',
     v: '20180323'
   }
   return $.getJSON(FOURSQUARE_VENUE_SEARCH_URL+`${restaurantID}`, query, (data) => callback(data, index))
   .fail(function() {
-    console.log( "error" );
+    //if API quota limit is reached, will instead populate with placeholder image and hours
     handlesVenuePlaceholderData();
   })
 }
-
-
 
 // Handling FOURSQUARE API---Name
 function getDataFromFOURSQUAREApi(zipCode, restaurantCatagory, callback){
@@ -201,25 +145,58 @@ function getDataFromFOURSQUAREApi(zipCode, restaurantCatagory, callback){
   $.getJSON(FOURSQUARE_SEARCH_URL, query, callback);
 }
 
-function renderRestaurantResult(result) {  
-  return `
-  <div role='button' class='restaurant-results' data-hours='${result.hours}' data-description='${result.description}'>
-    <h4>${result.venue.name}</h4>
-    <p>${result.venue.location.address}</p>
-    <img src='${result.img}' alt=''> 
-  </div>
-  `;
+function handlesRestaurantData(data){
+  const groupsObj = data.response.groups[0];
+  restaurantData = groupsObj.items;
+  getRestaurantPhotos();
 }
 
+function getRestaurantPhotos() {
+  const requests = restaurantData.map(function(restaurant, index){
+    const restaurantID = restaurantData[index].venue.id;
+    return getDataFromFOURSQUAREVENUEApi(restaurantID, index, handlesVenueData);
+  })
+    $.when(...requests).done(() => {
+    displayFOURSQUARESearchData();
+  }) 
+}
+
+function handlesVenueData(data, index) {
+  restaurantData[index].img = `${data.response.venue.bestPhoto.prefix}100x100${data.response.venue.bestPhoto.suffix}`;
+  if (data.response.venue.hours !== undefined) {
+    restaurantData[index].hours = data.response.venue.hours.timeframes.map((hourInfo) => renderHours(hourInfo));
+  } else {
+  restaurantData[index].hours = `Restaurant did not provide hours`;
+  }
+}
+
+function renderHours(hourInfo) {
+  return `${hourInfo.days}: ${hourInfo.open[0].renderedTime}`
+}
 
 function displayFOURSQUARESearchData() {
   const results = restaurantData.map((item, index)=>renderRestaurantResult(item));
   $('.js-restaurant-result h4').html(results);
 }
 
+function renderRestaurantResult(result) {
+  const hoursObj = JSON.stringify(result.hours); 
+  return `
+  <div role='button' class='restaurant-results' data-hours='${hoursObj}' data-description='${result.description}'>
+    <h4>${result.venue.name}</h4>
+    <img class='restaurant-image' style='height:185px;width:185px'src='${result.img}' alt='restaurant'>
+    <p>${result.venue.location.address}</p> 
+  </div>
+  `;
+}
 
-
-
+function handlesVenuePlaceholderData() {
+  restaurantData.map(function(restaurant, index) {
+    restaurantData[index].img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Sinnbild_Autobahngasthaus.svg/100px-Sinnbild_Autobahngasthaus.svg.png';
+    restaurantData[index].hours = `check back later for hours`;
+  });
+  displayFOURSQUARESearchData();
+}
 
 // Displaying initial results
 function showResultsinDOM(zipCode) {
@@ -229,18 +206,16 @@ function showResultsinDOM(zipCode) {
   $('.chosen-section .choose-genre').html('Genre:');
   $('.chosen-section .choose-location').html('Location:');
   $("label[for='js-zip-code-query']").hide();
-  $('.js-zip-code-query').replaceWith(zipCode);
+  $('.js-zip-code-query').replaceWith(`<div class='zipcode-chosen'><p>${zipCode}</p></div>`);
   $('.js-search-form button.submit').hide();
   $('.edit').show();
   $('.find-out-more').prop('hidden', false);
 }
 
-
 // Clicking form submission
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    
     //Restaurant Results
     let genreQueryTarget = $(event.currentTarget).find("input[type='radio']:checked");
     const genre = genreQueryTarget.val();
@@ -248,26 +223,9 @@ function watchSubmit() {
     const zipCode = zipCodeQueryTarget.val();
     const restaurantCatagory = genreToFoodCatagory[genre];
     getDataFromFOURSQUAREApi(zipCode, restaurantCatagory, handlesRestaurantData);
-
     //Movie Results
     getDataFromGRACENOTEApi(zipCode, handleMovieData);
-
     showResultsinDOM(zipCode);
-  });
-}
-
-// handling edititing choices
-function handleEditChoices(){
-  $('.edit').on('click', function(event){
-    event.preventDefault();
-    $('.genre-radio-button').not('.genre-selected').show();
-    $('.js-search-form button.submit').show();
-    $('.chosen-zip-wrapper').html(`
-        <h2 class='choose-location'>Location</h2>
-          <label for="js-zip-code-query">Zip Code:</label>
-          <input class="js-zip-code-query" type="text" value='97214' required><br>`
-          )
-    $('.edit').hide();
   });
 }
 
@@ -281,6 +239,21 @@ function highlightChosenGenre(){
     targetGenre.toggleClass('genre-selected')
     $(targetGenre).find('input').prop('checked', true);
   })
+}
+
+// handling edititing choices
+function handleEditChoices(){
+  $('.edit').on('click', function(event){
+    event.preventDefault();
+    $('.genre-radio-button').not('.genre-selected').show();
+    $('.js-search-form button.submit').show();
+    $('.chosen-zip-wrapper').html(`
+        <h2 class='choose-location'>Location</h2>
+          <label for="js-zip-code-query">Zip Code:</label>
+          <input class="js-zip-code-query" id="js-zip-code-query" type="text" required><br>`)
+    $('.edit').hide();
+    $('.error-section').hide(); 
+  });
 }
 
 //Toggling selected Movie
@@ -305,66 +278,13 @@ function handleFinalRestaurantSelect(){
   });
 }
 
-
 //Giving more info of chosen movie and restaurant
-  //Restaurant
-function renderRestaurantHours(){}
-
-function renderRestaurantMoreInfo() {
-  // const restaurantAddress = $('.restaurant-results.result-selected').data('address');
-  const restaurantHours = $('.restaurant-results.result-selected').data('hours');
-
-  const restaurantName = $('.restaurant-results.result-selected h4').html();
-  console.log(restaurantName);
-  let genreQueryTarget = $('form').find("input[type='radio']:checked");
-  let genre = genreQueryTarget.val();
-  const restaurantCatagory = genreToFoodCatagory[genre];
-  
-  if(genre == 'Children'){
-    genre = 'Family';
-  }
-
-  if(genre == 'Fantasy'){
-    genre = 'Romance';
-  }
-
-  return `
-  <div>
-    <h4>${restaurantName}</h4>
-    <p>Known as one of your areas top ${restaurantCatagory} restaurants, we think this pairs best with a ${genre} movie</p>
-    <p>${restaurantHours}</p>
-  </div>
-  `;
-}
-
-function displayRestaurantMoreInfo() {
-  $('.restaurant-more-info').html(renderRestaurantMoreInfo());
-}
-
-  //Movie
-function  renderMovieMoreInfo() {
-  const movieSummary = $('.movie-results.result-selected').data('summary');
-  const movieTitle = $('.movie-results.result-selected h4').html();
-  // getDataFromGRACENOTEApi(displayShowtimes);
-  return `
-  <div>
-    <h4>${movieTitle}</h4>
-    <p>${movieSummary}</p>
-  </div>
-  `;
-}
-
-function displayMovieMoreInfo() {
-  $('.movie-more-info').html(renderMovieMoreInfo());
-}
-
-
-
+    //Handling clicking on find out more button
 function handleFindOutMore(){
   $('.find-out-more').on('click', function() {
     const movieChosen = $('.movie-results.result-selected h4').html();
     const restaurantChosen = $('.restaurant-results.result-selected h4').html();
-    //If both things are selected
+    //Checking to make sure on movie and on restaurant is chosen and giving error if not
     if ( (movieChosen !== undefined) && (restaurantChosen !== undefined) ) {
       //return more info on selected movie and restaurant
       $('.modal').css('display', 'block');
@@ -372,6 +292,7 @@ function handleFindOutMore(){
       displayMovieMoreInfo();
       displayRestaurantMoreInfo();
       displayShowtimes();
+      displayRestaurantHours();
       $('.js-select-error p').hide();
       const venueIDquery = $('result-selected').data('venueID');
       // give error paragraph if both aren't chosen
@@ -380,8 +301,67 @@ function handleFindOutMore(){
   }});
 }
 
-const theatres = [];
-let theatreCounter = 0;
+  //Restaurant
+function displayRestaurantMoreInfo() {
+  $('.restaurant-more-info').html(renderRestaurantMoreInfo());
+}
+
+function renderRestaurantMoreInfo() {
+  // const restaurantHours = $('.restaurant-results.result-selected').data('hours');
+  const restaurantName = $('.restaurant-results.result-selected h4').html();
+  let genreQueryTarget = $('form').find("input[type='radio']:checked");
+  let genre = genreQueryTarget.val();
+  const restaurantCatagory = genreToFoodCatagory[genre];
+  // Adjusting genre search term to genre
+  if(genre == 'Children'){
+    genre = 'Family';
+  }
+  if(genre == 'Fantasy'){
+    genre = 'Romance';
+  }
+  return `
+  <div>
+    <h4>${restaurantName}</h4>
+    <p>Known as one of your areas top ${restaurantCatagory} restaurants, we think this pairs best with a ${genre} movie</p>
+  </div>
+  `;
+}
+
+function displayRestaurantHours(){
+  const hoursArr = $('.restaurant-results.result-selected').data('hours');
+  const results = hoursArr.map((segment) => renderRestaurantHours(segment))
+  $('.restaurant-hours').html(results);
+}
+
+function renderRestaurantHours(segment){
+  return `
+    <p>${segment}</p>
+  `
+}
+
+  //Movie
+function displayMovieMoreInfo() {
+  $('.movie-more-info').html(renderMovieMoreInfo());
+}
+
+function  renderMovieMoreInfo() {
+  const movieSummary = $('.movie-results.result-selected').data('summary');
+  const movieTitle = $('.movie-results.result-selected h4').html();
+  return `
+  <div>
+    <h4>${movieTitle}</h4>
+    <p>${movieSummary}</p>
+  </div>
+  `;
+}
+
+function displayShowtimes(){
+  const showtimesArr = $('.movie-results.result-selected').data('showtimes');
+  const showtimes = showtimesArr.map((item) => renderShowtimes(item));
+  $('.showtimes').html(showtimes);
+  theatres = [];
+}
+
 function renderShowtimes(result){
 // Transform data into AMPM showtimes
   const militaryTime = result.dateTime.slice(11);
@@ -398,7 +378,7 @@ function renderShowtimes(result){
   }
   displayTime += (minutes < 10) ? ":0" + minutes : ":" + minutes;  
   displayTime += (hours >= 12) ? " PM" : " AM";
-  
+  //Results will return at most three theatres
   while ( (theatres.length < 3)  || (theatres.includes(`${result.theatre.name}`))){
     if (!(theatres.includes(`${result.theatre.name}`))    ) {
       theatres.push(`${result.theatre.name}`);
@@ -411,15 +391,7 @@ function renderShowtimes(result){
       <p class='showtimeTime'>${displayTime}</p>`;
       } 
     };
-
   }
-
-function displayShowtimes(){
-  const showtimesArr = $('.movie-results.result-selected').data('showtimes');
-  const showtimes = showtimesArr.map((item) => renderShowtimes(item));
-  $('.showtimes').html(showtimes);
-}
-
 
 //Closing the Modal
 function handleCloseModal(){
@@ -427,9 +399,6 @@ function handleCloseModal(){
     $('.modal').css('display', 'none');
   });
 }
-
-
-
 
 function init() {
   $(handleEditChoices);
